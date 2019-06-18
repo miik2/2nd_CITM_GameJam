96b37cd2f97a7c6ee89578210a29841b70d6c2ca
Òscar Faura Gràcia <36189245+ofaura@users.noreply.github.com>
2019-06-18 12:30:21 +0200

    player movement

---
+++ CITM_2nd_GameJam/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody2D player;
+
+    // Start is called before the first frame update
+    void Start()
+    {	
+		player = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        player.AddForce(movement * speed);
+    }
+}

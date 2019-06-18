54e990c907fb69b861ca8d1221e99f53bcbfddc6
sherzock <jacobo.galofre@gmail.com>
2019-06-18 21:25:26 +0200

    Transition

---
+++ CITM_2nd_GameJam/Assets/Scripts/FadeOutScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeOutScript : MonoBehaviour
+{
+
+    public Image fadeblack;
+
+    void Start()
+    {
+        fadeblack.canvasRenderer.SetAlpha(1.0f);
+
+    }
+
+    void Update()
+    {
+            fadeIn();
+    }
+
+    void fadeIn()
+    {
+        fadeblack.CrossFadeAlpha(0, 2, false);
+    }
+
+}

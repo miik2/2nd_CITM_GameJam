576cd3ef02265b1f0f3d1ed916b368493bedb827
sherzock <jacobo.galofre@gmail.com>
2019-06-18 16:51:40 +0200

    Intro and game

---
+++ CITM_2nd_GameJam/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed;
+
+    private int SceneManagerBoolean;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManagerBoolean = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y, -10);
+
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //if (SceneManagerBoolean == 1)
+            //{
+            //    SceneManagerBoolean = 2;
+            //}
+            //else
+            //{
+                SceneManager.LoadScene("Ingame");
+            //}
+
+        }
+    }
+}

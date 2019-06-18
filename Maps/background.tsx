36115127a259e7411940193eb460278912cca016
Dani Lorenzo <36265574+DLorenzoLaguno17@users.noreply.github.com>
2019-06-18 16:13:04 +0200

    created map

---
+++ Maps/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="background" tilewidth="32" tileheight="32" tilecount="72" columns="9">
+ <image source="background.png" width="288" height="256"/>
+</tileset>
---
+++ Maps/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="32" tileheight="32" margin="16" tilecount="280" columns="14">
+ <image source="tiles.png" width="480" height="656"/>
+</tileset>

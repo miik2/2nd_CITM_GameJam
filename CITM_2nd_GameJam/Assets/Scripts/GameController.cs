0971de3be250ac579235e5e02e44e11765866fab
Dani Lorenzo <36265574+DLorenzoLaguno17@users.noreply.github.com>
2019-06-19 09:33:21 +0200

    added game controller and explosions are now destroyed

--- CITM_2nd_GameJam/Assets/Scripts/GameController.cs
+++ CITM_2nd_GameJam/Assets/Scripts/GameController.cs
@@ -1,20 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;using System.Collections;
 
 public class GameController : MonoBehaviour
 {
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
-	void Start(){
-		SpawnWaves();
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
 	}
 
-	void SpawnWaves()
+	IEnumerator SpawnWaves ()
 	{
-		Vector3 spawnPosition = new Vector3 (spawnValues.x, Random.Range(-spawnValues.y, spawnValues.y), spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
+		yield return new WaitForSeconds(startWait);
+
+		while (true){
+			for (int i = 0; i <= hazardCount ; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 }

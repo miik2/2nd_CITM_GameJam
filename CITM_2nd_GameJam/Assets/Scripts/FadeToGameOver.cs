33d1c3122af324de278284bb3f4fcf45b91efb0a
Dani Lorenzo <36265574+DLorenzoLaguno17@users.noreply.github.com>
2019-06-19 13:11:43 +0200

    created game over scene

---
+++ CITM_2nd_GameJam/Assets/Scripts/FadeToGameOver.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeToGameOver : MonoBehaviour
+{
+
+	public Image fadeblack;
+	public float timetofade;
+	private float timeleft;
+	private bool startcounter;
+
+	void Start ()
+	{
+		fadeblack.canvasRenderer.SetAlpha(0.0f);
+		timeleft = timetofade;
+		startcounter = false;
+
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			fadeIn();
+			startcounter = true;
+
+		}
+		else
+		{
+			fadeblack.canvasRenderer.SetAlpha(0.0f);
+		}
+
+		if(startcounter == true)
+		{
+			timeleft -= Time.deltaTime;
+		}
+
+		if(timeleft <0)
+			SceneManager.LoadScene("MainMenu");
+
+
+	}
+
+	void fadeIn()
+	{
+		fadeblack.CrossFadeAlpha(1, timetofade, false);
+	} 
+
+}

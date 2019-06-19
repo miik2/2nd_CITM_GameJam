1c0881d09a82c10715ddc83bee3b20c4a9b281cc
Òscar Faura Gràcia <36189245+ofaura@users.noreply.github.com>
2019-06-19 08:55:14 +0200

    collisions

--- CITM_2nd_GameJam/Assets/Scripts/DestroyByContact.cs
+++ CITM_2nd_GameJam/Assets/Scripts/DestroyByContact.cs
@@ -6,25 +6,36 @@ public class DestroyByContact : MonoBehaviour
 	public GameObject explosion;
 	public GameObject playerExplosion;
 
-	void OnTriggerEnter(Collider other) 
+	void OnTriggerEnter2D(Collider2D other) 
 	{
-		if (other.tag == "Boundary")
-		{
-			return;
-		}
-
-		Instantiate(explosion, transform.position, transform.rotation);
-
-		if(other.tag == "Player"){
-		Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
-		}
-
+        if (gameObject.tag == "Enemy" && other.gameObject.tag == "Enemy")
+        {
+            return;
+        }
         else
         {
-            Score_Script.AddScore(10);
-        }
+            if (other.tag == "Boundary")
+            {
+                return;
+            }
 
-		Destroy(other.gameObject);
-		Destroy(gameObject);
-	}
+            Instantiate(explosion, transform.position, transform.rotation);
+
+            if (other.tag == "Player")
+            {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            }
+
+            else
+            {
+                Score_Script.AddScore(10);
+            }
+
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+            
+        
+    }
+		
 }
